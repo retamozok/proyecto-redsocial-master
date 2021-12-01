0891827b132863f132f1076073c68e8726fb3f86
retamozok <matiasagustin.1823@gmail.com>
2021-12-01 11:29:29 -0300

    Cambios - 1/12/2021

--- src/controllers/controllerPublicaciones.js
+++ src/controllers/controllerPublicaciones.js
@@ -12,7 +12,6 @@ export const create = async (req,res)=>{
   let nuevaPublicacion = req.body
   nuevaPublicacion.id = Math.floor(Math.random()*1000000000)
   console.log(nuevaPublicacion)
-  nuevaPublicacion.foto = nuevaPublicacion.id + ".png" 
   publicaciones.push(nuevaPublicacion) 
 
   res.status(200).redirect('/instagram')
